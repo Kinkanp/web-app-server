c2b6660a0ade21c9ecc241bf190c74790bdf2d8b
Ivan Romanenko <ivan.romanenko7415@gmail.com>
2024-04-11 12:28:38 +0300

    adjust prod compose file

--- api/packages/http-server/vitest.config.ts
+++ api/packages/http-server/vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     dir: './src',
-    cache: { dir: '../../node_modules/.ioc' }
+    cache: { dir: '../../node_modules/.http-server' }
   },
 })
